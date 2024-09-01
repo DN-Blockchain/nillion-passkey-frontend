02d36b1cf057e39fbc433293405f034d9f9d3b83
htphong01 <thanhphonga4.hbt@gmail.com>
2024-09-01 09:57:56 +0700

    add landing page

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { logout } from '@/api/auth';
+import { generatePasskeyOptions, verifyRegistration } from '@/api/passkey';
+import { getProfile } from '@/api/profile';
+import FingerPrintImg from '@/assets/fingerprint-white.svg';
+import { clearToken } from '@/utils/local-storage';
+import { create } from '@github/webauthn-json';
+import React, { useEffect, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { Link, useNavigate } from 'react-router-dom';
+import './style.scss';
 
 export default function Landing() {
+  const navigate = useNavigate();
+
+  const handleNavigate = (to) => {
+    navigate(to);
+  };
+
   return (
-    <div>Landing</div>
-  )
+    <div className="container">
+      <h1>Welcome to Nillion Passkey</h1>
+      <form>
+        <button type="button" onClick={() => handleNavigate('/register')}>Create new account</button>
+        <button type="button" onClick={() => handleNavigate('/login')}>Login to your account</button>
+      </form>
+    </div>
+  );
 }
